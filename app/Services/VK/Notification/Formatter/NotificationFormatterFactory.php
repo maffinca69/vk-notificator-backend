d74014c6257feb111064f5c3941e9d3504e23804
Vyacheslav Bazhenov <maffinca69@gmail.com>
2022-10-22 03:34:21 +0400

    Hot fix crash

--- app/Services/VK/Notification/Formatter/NotificationFormatterFactory.php
+++ app/Services/VK/Notification/Formatter/NotificationFormatterFactory.php
@@ -25,6 +25,7 @@ class NotificationFormatterFactory
         return match ($notification->getType()) {
             NotificationTypesDictionary::LIKE_VIDEO_TYPE,
             NotificationTypesDictionary::LIKE_PHOTO_TYPE,
+            NotificationTypesDictionary::LIKE_COMMENT_PHOTO_TYPE,
             NotificationTypesDictionary::LIKE_COMMENT_TYPE => $this->container->get(NotificationLikeFormatter::class),
             NotificationTypesDictionary::FOLLOW_TYPE => $this->container->get(NotificationFollowFormatter::class),
             NotificationTypesDictionary::FRIEND_ACCEPTED_TYPE => $this->container->get(NotificationFriendAcceptedFormatter::class),
--- app/Services/VK/Notification/Formatter/NotificationLikeFormatter.php
+++ app/Services/VK/Notification/Formatter/NotificationLikeFormatter.php
@@ -80,6 +80,7 @@ class NotificationLikeFormatter implements NotificationFormatterInterface
         $parent = $notification->getParent();
 
         return match ($type) {
+            NotificationTypesDictionary::LIKE_COMMENT_PHOTO_TYPE => 'ваш комментарий',
             NotificationTypesDictionary::LIKE_COMMENT_TYPE => 'ваш комментарий',
             NotificationTypesDictionary::LIKE_PHOTO_TYPE => 'вашу фотографию',
             NotificationTypesDictionary::LIKE_VIDEO_TYPE => 'вашу ' . $this->videoUrlFormatter->format($parent),
