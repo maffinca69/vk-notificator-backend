4a9f5e234043e534ad290695c5f8ffa9721b9b4f
Vyacheslav Bazhenov <maffinca69@gmail.com>
2022-11-19 15:09:32 +0400

    CI/CD

--- deploy.php
+++ deploy.php
@@ -9,24 +9,50 @@ option('ip-address', 'ip', InputOption::VALUE_OPTIONAL, 'Adds the cache tables t
 option('user', 'user', InputOption::VALUE_OPTIONAL, 'Adds the cache tables to the backup command.', false);
 option('deploy-path', 'path', InputOption::VALUE_OPTIONAL, 'Adds the cache tables to the backup command.', false);
 
-// Config
+// Project name
+set('application', 'VK Notificator');
 
+// Project repository
 set('repository', 'https://github.com/maffinca69/vk-notificator-backend');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
-
-task('deploy:test', function () {
+task('deploy:prepared', function () {
+    // Hosts
     host(input()->getOption('ip-address'))
-        ->set('remote_user', input()->getOption('user'))
+        ->user(input()->getOption('user'))
         ->set('deploy_path', input()->getOption('deploy-path'));
 });
 
-// Hosts
+task('artisan:cache:clear', function () {
+    run('{{bin/php}} {{release_path}}/artisan cache:clear');
+})->desc('Execute artisan cache:clear');
 
 
+task('artisan:config:cache', function() {})->setPrivate();
+task('artisan:down', function() {})->setPrivate();
+task('artisan:event:cache', function() {})->setPrivate();
+task('artisan:event:clear', function() {})->setPrivate();
+task('artisan:horizon:terminate', function() {})->setPrivate();
+task('artisan:optimize', function() {})->setPrivate();
+task('artisan:optimize:clear', function() {})->setPrivate();
+task('artisan:route:cache', function() {})->setPrivate();
+task('artisan:storage:link', function() {})->setPrivate();
+task('artisan:up', function() {})->setPrivate();
+task('artisan:view:cache', function() {})->setPrivate();
+task('artisan:view:clear', function() {})->setPrivate();
 
-// Hooks
-
-after('deploy:failed', 'deploy:unlock');
+// Tasks
+task('deploy', [
+    'deploy:prepared',
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:cache:clear',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
